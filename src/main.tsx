d2d5d33c2c5d50e9092cbe94f16b2a824d098d7d
Foldora AI <foldora-ai@users.noreply.github.com>
2026-05-11 12:55:10 +0000

    tryit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-// import App from "./App.tsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")!).render(<App />);
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App.tsx";
 import "./index.css";
+
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
